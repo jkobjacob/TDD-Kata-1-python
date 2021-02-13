1108d380d6d54acdec92e68f944dfc30d8917d02
Jkob-Jacob <sherinjacob20@gmail.com>
2021-02-13 09:43:39 +0530

    Added functionality to handle input string with two or more chars.

--- src/StringCalculator.py
+++ src/StringCalculator.py
@@ -1,10 +1,14 @@
+from functools import reduce
+
 class StringCalculator():
     def __init__(self):
-        pass
+        self.delimiter = ","
 
     def add(self,ip_str):
         if ip_str == "":
             return 0
 
         else:
-            return int(ip_str)
+            return reduce(lambda acc, num: acc + int(num),
+                        ip_str.split(self.delimiter),
+                        0)
--- test/TestStringCalculator.py
+++ test/TestStringCalculator.py
@@ -12,3 +12,6 @@ class TestStringCalculator(unittest.TestCase):
 
     def test_for_string_input_with_one_char(self):
         self.assertEqual(1,self.stringCalculator.add("1"))
+
+    def test_for_string_input_with_more_than_one_chars(self):
+        self.assertEqual(6,self.stringCalculator.add("1,2,3"))
